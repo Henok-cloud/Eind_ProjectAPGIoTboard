6e7b3386f5df2ca40c46b007082a67777905590d
Henok Mulugeta <henok.mulugetameconen@student.ap.be>
2025-08-08 21:23:16 +0200

    internal_temp toevoeging omzetten naar 6 bit two's complement, toont bits op LEDs 3-8, debug

--- internal_temp.py
+++ internal_temp.py
@@ -6,6 +6,7 @@ from machine import ADC
 
 BTN1  = 1
 BTN2  = 2
+LED_START = 3 
 LONG_ms = 2000
 LED_count = 6 
 
@@ -34,3 +35,28 @@ def rd_intern_temp():
     return temp_celsius
 tempC = rd_intern_temp()
 print("internal Tempratuur", tempC, "°C")
+
+def run():
+    apgiotboard.all_leds_off()
+    while True:
+        if exit_pressed():
+            apgiotboard.all_leds_off()
+            return
+        
+        # meet temp
+        temp = rd_intern_temp()
+        t_int = int(round(temp)) #afronden
+
+        # omzetten naar 6 bit two's complement
+        val = t_int & ((1<<LED_count)-1)
+        bits = format(val,f'0{LED_count}b')
+
+        # toont bits op LEDs 3-8
+        for i, bit in enumerate(reversed(bits)):
+            led = apgiotboard.led(LED_START + 1)
+            led.on() if bit == '1' else led.off()
+
+        # debug 
+        print(f"Temp:{temp:.2f} °C {bits}")
+
+        time.sleep(1)
